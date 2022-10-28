d58f9f7034783d4ec2e63301e64f246e746b8b06
Oliver Song <me@oliversong.net>
2022-10-27 22:37:41 -0700

    oh boy

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const config = require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
@@ -7,7 +8,21 @@ app.set("views", __dirname + "/views")
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    secret: config.parsed.API_KEY,
+    table1Image: '',
+    table2Image: '',
+    table3Image: '',
+    table4Image: '',
+    table5Image: '',
+    table6Image: '',
+    table7Image: '',
+    table8Image: '',
+    table9Image: '',
+    table10Image: '',
+    table11Image: '',
+    table12Image: '',
+  })
 })
 
 app.listen(port, () => {
---
+++ public/scripts/index.js
@@ -0,0 +1,61 @@
+
+const onClick = async function() {
+  // post to openai API
+  const input = $('#prompt').val();
+  const tableNumber = $('#tableInput').val();
+  if (!input) {
+    alert('No input provided');
+  } else {
+    $('.spinnerContainer').css("display", "flex");
+  }
+
+  const response = await fetch("https://api.openai.com/v1/images/generations", {
+    method: "POST",
+    body: JSON.stringify({
+      model: "image-alpha-001",
+      prompt: input,
+      num_images: 1,
+      size: "1024x1024",
+      response_format: "url"
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Basic " + btoa("bearer:" + jank_secret),
+    }
+  })
+
+  $('.spinnerContainer').css("display", "none");
+
+  // wait for result
+  if (!response.ok) {
+    throw new Error(`Request failed with status ${reponse.status}`)
+    alert('Something went wrong, complain to Oliver');
+  }
+  // get url from result
+  const result = await response.json()
+  const url = result.data[0].url;
+
+  const response = await fetch("/submit", {
+    method: "POST",
+    body: JSON.stringify({
+      url: url,
+      table: tableNumber,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    }
+  });
+}
+
+$('.generate').click(onClick);
+$('.table-entry.filled').click(function(e) {
+  const url = e.target.getAttribute('src');
+
+  // display on screen
+  $('#result').attr('src', url);
+  $('#result').css("display", "flex");
+})
+
+$('#result').click(function() {
+  $('#result').css("display", "none");
+})
